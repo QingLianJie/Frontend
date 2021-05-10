1c380b633f089fcf4509b86266035c5cd4f7d87a
LFN <liangfengning@foxmail.com>
2021-05-10 15:43:04 +0800

    格式化代码

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Qinglianjie 清廉街
+#### 清廉街
 
 Powered by Svelte Kit.
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,17 @@
-import preprocess from 'svelte-preprocess';
+import vercel from '@sveltejs/adapter-vercel'
+import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
-};
+  kit: {
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte',
+    adapter: vercel(),
+  },
+}
 
-export default config;
+export default config
