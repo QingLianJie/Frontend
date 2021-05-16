f136d915050925dd557072788329da4dbbd247b5
LFN <liangfengning@foxmail.com>
2021-05-17 00:07:10 +0800

    修改 Footer 和首页样式

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -7,14 +7,29 @@ const Footer = ({ fill }: IFooterProps) => {
       as="footer"
       minH="24"
       px="4"
-      py="6"
-      flexDirection="column"
-      alignItems="center"
-      justifyContent="flex-end"
+      py="8"
+      alignItems="flex-end"
+      justifyContent="space-between"
       flex={fill ? '1' : 'initial'}
+      flexWrap="wrap"
     >
-      <Text fontSize="md" color="gray.500" fontWeight="bold" py="4">
+      <Text
+        fontSize="sm"
+        color="gray.500"
+        fontWeight="bold"
+        py="1"
+        lineHeight="1.75"
+      >
         Working In Progress
+      </Text>
+      <Text
+        fontSize="sm"
+        color="gray.500"
+        fontWeight="bold"
+        py="1"
+        lineHeight="1.75"
+      >
+        当前为测试版本，不代表最终成果
       </Text>
     </Flex>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import Main from '../components/Main'
 import { homeLink } from '../data/links'
 import meta from '../data/meta'
 
-const items = Array.from(Array(10), () => {
+const items = Array.from(Array(9), () => {
   const text = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Etiam sagittis velit eu placerat pretium. Phasellus blandit magna in leo lobortis sagittis. Nunc interdum facilisis augue, ac imperdiet purus porttitor id. Vivamus ut neque vel leo vulputate efficitur. Donec luctus, urna ac tempus rutrum, nunc ipsum tristique ante, nec viverra turpis eros vitae turpis. Praesent congue a odio faucibus aliquam. Etiam et quam nunc. Fusce posuere molestie efficitur. Suspendisse ut dolor lacus. Nullam finibus nunc magna, ut vulputate nibh accumsan quis. Proin egestas non dolor et tincidunt. Aenean semper eros quam, ac volutpat purus fermentum in. In accumsan mollis nunc facilisis faucibus. Etiam non interdum ipsum, vitae euismod dui. Fusce sagittis condimentum ullamcorper. Phasellus sit amet dictum tellus, vel facilisis nulla.`
 
   const sentence = text.split('. ')
@@ -34,8 +34,8 @@ const IndexPage = () => {
         <Grid
           templateColumns="repeat(auto-fit, 84px)"
           justifyContent="center"
-          pt={{ base: 0, lg: 2 }}
-          pb={{ base: 0, md: 8, lg: 12 }}
+          pt={{ base: 0, md: 2, lg: 4 }}
+          pb={{ base: 0, md: 6, lg: 8 }}
           gap={{ base: 0, lg: 1 }}
         >
           {homeLink.map(link => (
