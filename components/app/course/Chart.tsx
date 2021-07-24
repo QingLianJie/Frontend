83069f490897998f08050898570022808293e2d5
LFN <liangfengning@foxmail.com>
2021-07-24 21:25:23 +0800

    添加课程成绩时间切换

--- components/app/course/Chart.tsx
+++ components/app/course/Chart.tsx
@@ -1,14 +1,16 @@
 import {
   Box,
   Heading,
-  HStack,
   Icon,
   Select,
   Spacer,
+  Text,
   useTheme,
+  Wrap,
+  WrapItem,
 } from '@chakra-ui/react'
 import { Axis, Chart, Interval, Tooltip } from 'bizcharts'
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { RiBarChartBoxLine } from 'react-icons/ri'
 import useCourse from '../../../hooks/useCourse'
 import { calcChartData } from '../../../utils/calc/course-chart'
@@ -23,6 +25,7 @@ const CourseChart = ({ id }: CourseChartProps) => {
   const theme = useTheme()
   const { courseInfo, isLoading, isError } = useCourse(id)
   const [chartData, setChartData] = useState<CourseStatChartData[]>([])
+  const [select, setSelect] = useState('')
 
   useEffect(() => {
     if (courseInfo) {
@@ -30,82 +33,115 @@ const CourseChart = ({ id }: CourseChartProps) => {
     }
   }, [courseInfo])
 
+  const handleSelectChange = (e: ChangeEvent) => {
+    const target = e.target as HTMLInputElement
+    setSelect(target.value)
+    setChartData(calcChartData(courseInfo, target.value || 'all'))
+  }
+
   return (
     <>
       <GroupContainer>
         {isError ? null : isLoading ? null : (
           <CardContainer>
-            <HStack spacing="4">
-              <Icon as={RiBarChartBoxLine} w="5" h="5" />
-              <Heading as="p" fontSize="lg" fontWeight="600">
-                成绩分布图
-              </Heading>
-              <Spacer />
+            <Wrap spacing="4" justify="space-between">
+              <WrapItem alignItems="center" py="1">
+                <Icon as={RiBarChartBoxLine} w="5" h="5" ms="1" me="3" />
+                <Heading as="p" fontSize="lg" fontWeight="600">
+                  成绩分布图
+                </Heading>
+              </WrapItem>
 
-              <Select placeholder="所有时间" maxW="44">
-                {courseInfo &&
-                  Object.keys(courseInfo.statistics).map((key, index) =>
-                    key === 'all' ? null : (
-                      <option key={index} value={key}>
-                        {key}
-                      </option>
-                    )
-                  )}
-              </Select>
-            </HStack>
-            <Spacer h="4" />
-            <Box p="4" w="full">
-              {chartData && (
-                <Chart
-                  height={400}
-                  autoFit
-                  data={chartData}
-                  interactions={['active-region']}
-                  scale={{
-                    score: { alias: '成绩', tickCount: 10 },
-                    count: { alias: '人数', tickCount: 6 },
-                  }}
+              <WrapItem>
+                <Select
+                  placeholder="所有时间"
+                  maxW="44"
+                  onChange={handleSelectChange}
                 >
-                  <Axis
-                    name="count"
-                    title
-                    label={{
-                      offset: 16,
-                      style: { fontSize: 14, fontFamily: 'Inter' },
+                  {courseInfo &&
+                    Object.keys(courseInfo.statistics).map((key, index) =>
+                      key === 'all' ? null : (
+                        <option key={index} value={key}>
+                          {key}
+                        </option>
+                      )
+                    )}
+                </Select>
+              </WrapItem>
+            </Wrap>
+            <Spacer h="6" />
+            <Box px={{ base: 0, md: 4 }} py={{ base: 2, md: 4 }} w="full">
+              {chartData && (
+                <>
+                  <Chart
+                    height={400}
+                    autoFit
+                    data={chartData}
+                    interactions={['active-region']}
+                    scale={{
+                      score: { alias: '成绩', tickCount: 10 },
+                      count: { alias: '人数', tickCount: 6 },
                     }}
-                    grid={{
-                      line: {
-                        type: 'line',
-                        style: { stroke: '#d9d9d9', lineWidth: 0.5 },
-                      },
-                    }}
-                  />
-                  <Axis
-                    name="score"
-                    title
-                    label={{
-                      offset: 16,
-                      style: { fontSize: 14, fontFamily: theme.fonts.body },
-                    }}
-                    tickLine={null}
-                  />
-                  <Interval position="score*count" />
-                  <Tooltip
-                    shared
-                    domStyles={{
-                      'g2-tooltip': {
-                        fontFamily: theme.fonts.body,
-                        fontSize: '14',
-                        lineHeight: '1',
-                        boxShadow: 'none',
-                        padding: '0.25rem 1rem',
-                      },
-                      'g2-tooltip-marker': { display: 'none' },
-                      'g2-tooltip-title': { fontWeight: '600' },
-                      'g2-tooltip-value': { marginLeft: '0.5rem' },
-                    }}
-                  />
-                </Chart>
+                  >
+                    <Axis
+                      name="count"
+                      label={{
+                        offset: 16,
+                        style: {
+                          fontSize: 14,
+                          fontFamily: 'Inter',
+                          fill: '#718096',
+                        },
+                      }}
+                      grid={{
+                        line: {
+                          type: 'line',
+                          style: { stroke: '#71809622' },
+                        },
+                      }}
+                    />
+                    <Axis
+                      name="score"
+                      label={{
+                        offset: 16,
+                        style: {
+                          fontSize: 14,
+                          fontFamily: theme.fonts.body,
+                          fill: '#718096',
+                        },
+                      }}
+                      tickLine={null}
+                      line={{
+                        style: { stroke: '#71809666' },
+                      }}
+                    />
+                    <Interval position="score*count" />
+                    <Tooltip
+                      shared
+                      domStyles={{
+                        'g2-tooltip': {
+                          fontFamily: theme.fonts.body,
+                          fontSize: '14',
+                          lineHeight: '1',
+                          boxShadow: 'none',
+                          padding: '0.25rem 1rem',
+                        },
+                        'g2-tooltip-marker': { display: 'none' },
+                        'g2-tooltip-title': { fontWeight: '600' },
+                        'g2-tooltip-value': { marginLeft: '0.5rem' },
+                      }}
+                    />
+                  </Chart>
+                  <Text
+                    w="full"
+                    pt="7"
+                    pb="1"
+                    textAlign="center"
+                    color="gray.500"
+                  >{`${select || '所有时间'} 的成绩分布图，统计人数 ${
+                    courseInfo.statistics[select || 'all'].total
+                  } 人`}</Text>
+                </>
               )}
             </Box>
           </CardContainer>
--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,20 +1,24 @@
-import { Flex, Text } from '@chakra-ui/react'
+import { Container, Flex, Text } from '@chakra-ui/react'
 
 const Footer = () => {
   return (
-    <Flex
-      as="footer"
-      px={{ base: '6', md: '8', lg: '12' }}
-      py="8"
-      justify="space-between"
-      align="flex-end"
-    >
-      <Text fontSize="sm" color="gray.500">
-        清廉街 © 2021
-      </Text>
-      <Text fontSize="sm" color="gray.500">
-        黑ICP备2021003925号-1
-      </Text>
+    <Flex as="footer" px={{ base: '6', md: '8', lg: '12' }} py="8">
+      <Container
+        w="full"
+        maxW="container.xl"
+        mx="auto"
+        d="flex"
+        px={{ base: 0, md: 4 }}
+        alignItems="center"
+        justifyContent="space-between"
+      >
+        <Text fontSize="sm" color="gray.500">
+          清廉街 © 2021
+        </Text>
+        <Text fontSize="sm" color="gray.500">
+          黑ICP备2021003925号-1
+        </Text>
+      </Container>
     </Flex>
   )
 }
