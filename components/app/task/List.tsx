4ff17cf766f307cbc545cf2bbca51c3d1d0f5a48
LFN <liangfengning@foxmail.com>
2021-08-14 12:59:14 +0800

    任务列表默认折叠

--- components/app/task/List.tsx
+++ components/app/task/List.tsx
@@ -1,8 +1,10 @@
 import {
   Badge,
+  Button,
   Center,
   Divider,
   HStack,
+  Icon,
   Spinner,
   Text,
   ThemeTypings,
@@ -11,6 +13,8 @@ import {
   Wrap,
   WrapItem,
 } from '@chakra-ui/react'
+import { useState } from 'react'
+import { RiArrowDropDownLine, RiArrowDropUpLine } from 'react-icons/ri'
 import ButtonLink from '../../../components/common/action/link/ButtonLink'
 import useTask from '../../../hooks/useTask'
 import useUser from '../../../hooks/useUser'
@@ -34,6 +38,7 @@ const statusColorMap: { [key in TaskStatus]: ThemeTypings['colorSchemes'] } = {
 const TaskList = () => {
   const { user, isError: isUserError, isLoading: isUserLoading } = useUser()
   const { tasks, isLoading, isError } = useTask()
+  const [more, setMore] = useState(false)
 
   return (
     <>
@@ -92,7 +97,7 @@ const TaskList = () => {
             {tasks.length === 0 ? (
               <Text>暂时没有任务，试试刷新课表或者获取成绩吧</Text>
             ) : (
-              tasks.map((task, index) => (
+              tasks.slice(0, more ? tasks.length : 8).map((task, index) => (
                 <Wrap key={index} w="full" spacing="3">
                   <WrapItem d="flex" alignItems="center">
                     <Tooltip
@@ -133,6 +138,20 @@ const TaskList = () => {
                 </Wrap>
               ))
             )}
+
+            <Button
+              isFullWidth
+              onClick={() => setMore(!more)}
+              variant="link"
+              size="sm"
+            >
+              {more ? '收起' : '查看全部'}
+              {more ? (
+                <Icon as={RiArrowDropUpLine} w="5" h="5" />
+              ) : (
+                <Icon as={RiArrowDropDownLine} w="5" h="5" />
+              )}
+            </Button>
           </VStack>
         </CardContainer>
       )}
