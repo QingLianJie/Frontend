4f10e87151b42256d4bb7aecaf77375a397c1b2b
LFN <liangfengning@foxmail.com>
2021-08-14 20:42:10 +0800

    修复编译错误

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { ChakraProvider, extendTheme } from '@chakra-ui/react'
-import { AppProps } from 'next/dist/next-server/lib/router/router'
+import { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../libs/markdown.css'
 
