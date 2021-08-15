007e261e01af5d4b0571300ecd257752de62778d
LFN <liangfengning@foxmail.com>
2021-08-15 16:06:45 +0800

    构建与开发环境调整

--- data/api-config.ts
+++ data/api-config.ts
@@ -1,2 +1,9 @@
-export const BASE_API_URL = 'https://api.qinglianjie.cn'
-export const BASE_AVATAR_URL = 'https://api.qinglianjie.cn'
+export const BASE_API_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost'
+    : 'https://api.qinglianjie.cn'
+
+export const BASE_AVATAR_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost'
+    : 'https://api.qinglianjie.cn'
